4de15cebe41da51f7ca65f3837d6b5a220e6853e
Natesuri <natesuri@gmail.com>
2018-10-12 15:35:00 -0400

    improved logic to toggle classes. Psuedocode for time events

--- game/js/main.js
+++ game/js/main.js
@@ -2,15 +2,19 @@
 const colorsAvail = [
   {
     colorId: 'red',
+    // class: 'red',
   },
   {
     colorId: 'blue',
+    // class: 'blue',
   },
   {
     colorId: 'green',
+    // class: 'green',
   },
   {
-    colorId: 'yellow'
+    colorId: 'yellow',
+    // class: 'yellow',
   },
 ];
 
@@ -20,7 +24,6 @@ let sequenceMin = 3;
 let sequenceCurrent = sequenceMin;
 //empty simonSequence that will be filled by createSequence function
 let simonSequence = [];
-let i = 0;
 
 ///function to create simon's sequence
 let createSequence = function () {
@@ -28,7 +31,7 @@ let createSequence = function () {
     //console.log("I work!");
 
     // assign randomly select colorId from colorsAvail and store in let simonColorSelect
-    let simonColorSelect = colorsAvail[Math.floor(Math.random() * 4)].colorId;
+    let simonColorSelect = colorsAvail[Math.floor(Math.random() * 4)];
     //console.log(simonColorSelect);
 
     //add random selection to Simon's sequence
@@ -36,15 +39,39 @@ let createSequence = function () {
   }
 };
 
-
+createSequence();
 
 
 
 //fuction to display simon's sequence
+  //js to change css of game elements
+let playSequence = function () {
+  for (let i = 0; i < simonSequence.length; i++) {
+    $('#' + simonSequence[i].colorId).toggleClass('bright');
+    // console.log(simonSequence[i].colorId + " is played!")
+    // Add a time interval to reset the class 500 miliseconds after the click, and then restart the loop
+
+  }
+}
+
+
+$('div').click(function() {
+  $(this).toggleClass('bright');
+  // Add a time interval to reset the class 500 miliseconds after the click
+});
+
+// Testing code to link colorsAvail array to Div.Id
+  // let toggledElement = document.getElementById('red');
+  // jQuery for above
+  // $('#red').toggleClass('bright');
+
 
 //fuction to assign simonSequence[i] = correctSelection
+
 //fuction to listen for user's selections, userSelection
 //fuction to compare userSelection to correctSelection and report if they match
 //function to fail the user if userSelection does not match the correctSelection
 //report user success if they finish loop without failing
 //add +1 to sequenceCurrent
+
+//add 1 more random color to the end
